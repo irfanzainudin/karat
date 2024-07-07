b7746ac9bf1ea213c0d3a67729c9e31baa0d8191
Irfan Zainudin <m.irfan.zain@gmail.com>
2024-07-07 12:52:13 +0800

    implemented a single neuron

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// TODO: move this to a separate file once it becomes too large
+struct Neuron {
+    bias: f64,
+    weights: Vec<f64>,
+    inputs: Vec<f64>,
+    output: f64
+}
+
 fn main() {
     println!("Hello, world! karat is a Rust library for machine learning.");
+
+    let singleNeuron = Neuron{
+        bias: 1.0,
+        weights: vec![],
+        inputs: vec![],
+        output: 0.0
+    };
 }
